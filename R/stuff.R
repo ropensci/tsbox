a5abb660898b3d4415a0d0ce9d10987dcec4320c
Christoph Sax <christoph.sax@gmail.com>
2017-03-07 20:33:42 +0100

    clean up

--- R/stuff.R
+++ R/stuff.R
@@ -11,47 +11,24 @@
 
 
 
-check_align <- function(x, y){
-  stopifnot(inherits(x, "ts"), inherits(y, "ts"))
-  stopifnot(frequency(x) == frequency(y))
-  f <- frequency(x)
-  if (abs(tsp(x)[2] + 1/f - tsp(y)[1]) > 10e-6){
-    stop("x and y are not aligned. x ends: ", 
-         paste(end(x), collapse = ":"), ". y starts: ", 
-         paste(start(y), collapse = ":")
-         )
-  }
-  return(TRUE)
-}
+
+# #' @export
+# LEVEL <- function(pc, x){
+#   check_align(x, pc)
+#   lx <- lag(x, -1)
+#   fctr <- pc
+#   fctr[] <- cumprod(1 + (pc / 100))
+#   fctr * lx[length(lx)]
+# }
 
 
-
-
-#' @export
-tsbind <- function(a, b, check = TRUE){
-   if (check) check_align(a, b)
-   z <- window(a, end = end(b), extend = TRUE)
-   window(z, start = start(b)) <- b
-   z
-}
-
-#' @export
-LEVEL <- function(pc, x){
-  check_align(x, pc)
-  lx <- lag(x, -1)
-  fctr <- pc
-  fctr[] <- cumprod(1 + (pc / 100))
-  fctr * lx[length(lx)]
-}
-
-
-#' @export
-#' @method as.matrix ts
-as.matrix.ts <- function(x){
-  z <- unclass(x)
-  attr(z, "tsp") <- NULL
-  as.matrix(z)
-}
+# #' @export
+# #' @method as.matrix ts
+# as.matrix.ts <- function(x){
+#   z <- unclass(x)
+#   attr(z, "tsp") <- NULL
+#   as.matrix(z)
+# }
 
 
 
