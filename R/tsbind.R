1b14b332529b26afb51e5cd4289e4014e31c7020
Christoph Sax <christoph.sax@gmail.com>
2017-03-08 21:48:33 +0100

    better way to label

--- R/tsbind.R
+++ R/tsbind.R
@@ -13,36 +13,29 @@
 #' @export
 tsbind <- function(...){
 
-
   ll <- list(...)
-
   desired.class <- desired_class(ll)
-
   ll.xts <- lapply(ll, as_xts)
 
-  lcnames <- lapply(ll.xts, colnames)
-
-  is.null.cnames <- vapply(lcnames, is.null, FALSE)
-
-  if (any(is.null.cnames)){
-    # good colnames
-    list.with.names <- lapply(substitute(placeholderFunction(...))[-1], deparse)
-    relevant.names <- list.with.names  # a template
-    if (!is.null(names(list.with.names))){
-      for (i in 1:length(list.with.names)){
-        if (names(list.with.names)[i] == ""){  # no manual specification        
-          relevant.names[[i]] <- list.with.names[[i]]
-        } else {                               # manual specification
+  list.with.names <- lapply(substitute(placeholderFunction(...))[-1], deparse)
+  relevant.names <- list.with.names  # a template
+  if (!is.null(names(list.with.names))){
+    for (i in 1:length(list.with.names)){
+      if (names(list.with.names)[i] == ""){  # no manual specification        
+        relevant.names[[i]] <- list.with.names[[i]]
+      } else {       
+        # manual specification, only if one series           
+        if (length(list.with.names[[i]]) == 1){
           relevant.names[[i]] <- names(list.with.names)[i]
-        }
+        } else {
+          relevant.names[[i]] <- list.with.names[[i]]
+        } 
       }
     }
-    lcnames[is.null.cnames] <- relevant.names[is.null.cnames]
   }
-
   
   z <- do.call("cbind", ll.xts)
-  colnames(z) <- unlist(lcnames)
+  colnames(z) <- unlist(relevant.names)
  
   as_(desired.class)(z)
 
