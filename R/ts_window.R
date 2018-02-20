afb6d85e0e1e2f356a5d73c399bcd768750444a6
Christoph <christoph.sax@gmail.com>
2018-02-20 18:05:11 +0100

    use anydate with dates

--- R/ts_window.R
+++ R/ts_window.R
@@ -17,14 +17,20 @@ ts_window <- function(x, start = NULL, end = NULL) {
   z <- ts_dts(x)
   ctime <- colname_time(z)
 
+  anyfun = if (class(z[[ctime]]) == "Date") {
+    function(x) anydate(x)
+  } else {
+    function(x) anytime(x)
+  }
+
   if (!is.null(start)) {
-    z <- filter_data.table(z, ctime, ">=", anytime(start))
+    z <- filter_data.table(z, ctime, ">=", anyfun(start))
   }
   if (!is.null(end)) {
     if (!is.null(start) && start >= end) {
       stop("'start' cannot be at or after 'end'", call. = FALSE)
     }
-    z <- filter_data.table(z, ctime, "<=", anytime(end))
+    z <- filter_data.table(z, ctime, "<=", anyfun(end))
   }
   z <- copy_class(z, x)
 
