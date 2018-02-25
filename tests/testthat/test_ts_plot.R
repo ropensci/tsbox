eb708fe17e2c310d7608c4ac7e1f06dd52bc2815
Christoph Sax <christoph.sax@gmail.com>
2018-02-25 01:58:01 +0100

    una mas

--- tests/testthat/test_ts_plot.R
+++ tests/testthat/test_ts_plot.R
@@ -6,15 +6,23 @@ context("ts_plot")
 
 test_that("ts_plot works", {
   ts_plot(AirPassengers, title = "AirPassengers", subtitle = "Heyhey")
-  tf <- tempfile()
+  tf <- tempfile(".pdf")
   ts_save(tf, device = "pdf", open = FALSE)
   expect_true(file.size(tf) > 3000)
+
+  tf <- tempfile(".png")
   ts_save(tf, device = "png", open = FALSE)
   expect_true(file.size(tf) > 3000)
+
+  tf <- tempfile(".bmp")
   ts_save(tf, device = "bmp", open = FALSE)
   expect_true(file.size(tf) > 3000)
+
+  tf <- tempfile(".jpeg")
   ts_save(tf, device = "jpeg", open = FALSE)
   expect_true(file.size(tf) > 3000)
+
+  tf <- tempfile(".tiff")
   ts_save(tf, device = "tiff", open = TRUE)
   expect_true(file.size(tf) > 3000)
 
