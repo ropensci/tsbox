2ba5b7b7f39c4b82665d11e29cf222d0ba109c0c
Christoph Sax <christoph.sax@gmail.com>
2018-03-18 13:56:26 +0100

    rolling joings in ts_arithmetic

--- R/ts_arithmetic.R
+++ R/ts_arithmetic.R
@@ -14,44 +14,59 @@ ts_arithmetic <- function(e1, e2, fun = `-`){
 
   cname <- dts_cname(z1)
   cname2 <- dts_cname(z2)
+  cid <- cname$id
 
   if (!identical(cname$id, cname2$id)){
     stop("id columns are not identical", call. = FALSE)
   }
-  if (!identical(cname$time, cname2$time)){
-    stop("time column is not identical", call. = FALSE)
-  }
-  if (!identical(cname$value, cname2$value)){
-    stop("value column is not identical", call. = FALSE)
-  }
 
-  # the following is quite standard, should be unified and factored out
   setnames(z1, cname$value, "value")
-  setnames(z2, cname$value, "value2")
+  setnames(z2, cname2$value, "value2")
   setnames(z1, cname$time, "time")
-  setnames(z2, cname$time, "time")
+  setnames(z2, cname2$time, "time")
 
-  is.posixct.z1 <- inherits(z1$time, "POSIXct")
-  is.posixct.z2 <- inherits(z1$time, "POSIXct")
+  if (length(cname$id) > 0){
 
-  if (is.posixct.z1 || is.posixct.z2){
-    tzone <- unique(c(dts_tattr(z1)$tzone), dts_tattr(z2)$tzone)[1]
-    z1[, time := as.integer(as.POSIXct(time))]
-    z2[, time := as.integer(as.POSIXct(time))]
+    if (length(cname$id) > 1){
+      sep.str <- "!%!#"
+      z1 <- combine_id_cols(z1, sep = sep.str)
+      z2 <- combine_id_cols(z2, sep = sep.str)
+      cid <- "id"
+    }
+
+    ll1 <- split(z1, z1[[cid]])
+    ll2 <- split(z2, z2[[cid]])
+
+    ll1 <- lapply(ll1, function(e) e[, (cid) := NULL])
+    ll2 <- lapply(ll2, function(e) e[, (cid) := NULL])
+
+    z <- rbindlist(Map(merge_time_date, x = ll1, y = ll2), idcol = cid)
+
+    # separate id cols
+    if (length(cname$id) > 1){
+      sep_id <- function(x){
+        spl <- strsplit(x$id, split = sep.str)
+        ids <- do.call(data.table, lapply(seq_along(cname$id), function(id) sapply(spl, function(e) e[id])))
+        setnames(ids, cname$id)
+        x$id <- NULL
+        cbind(ids, x)
+      }
+      z1 <- sep_id(z1)
+      z2 <- sep_id(z2)
+    }
+    
+  } else {
+    z <- merge_time_date(z1, z2)
   }
 
-  z <- merge(z1, z2, by = c(cname$id, "time"))
   z[, value := fun(value, value2)]
   z[, value2 := NULL]
-
-  if (is.posixct.z1 || is.posixct.z2){
-    z[, time := as.POSIXct(time, origin = "1970-01-01", tz = tzone)]
-  }
+  z <- z[!is.na(value)]
 
   setnames(z, "time", cname$time)
   setnames(z, "value", cname$value)
   setattr(z, "cname", cname)
-  copy_class(z, e1)
+  copy_class(z, e1)[]
 }
 
 
