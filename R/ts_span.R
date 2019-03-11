abe40ef80e2cd0864869cc3373273877b99ec037
Christoph <christoph.sax@gmail.com>
2019-03-11 19:43:20 +0100

    deprecate ts_start, ts_end

--- R/ts_span.R
+++ R/ts_span.R
@@ -181,17 +181,3 @@ frequency_one <- function(x) {
   }
   fm
 }
-
-
-#' @export
-#' @name ts_span
-ts_start <- function(x) {
-  x.dts <- ts_dts(x)
-  range(x.dts[[dts_cname(x.dts)$time]])[1]
-}
-#' @export
-#' @name ts_span
-ts_end <- function(x) {
-  x.dts <- ts_dts(x)
-  range(x.dts[[dts_cname(x.dts)$time]])[2]
-}
