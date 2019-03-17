fa0d74e33a72d6fdce908de2fa2dce3ffaba9c01
Christoph <christoph.sax@gmail.com>
2019-03-17 21:25:19 +0100

    oops

--- tests/testthat/test_arithmetic.R
+++ tests/testthat/test_arithmetic.R
@@ -28,10 +28,8 @@ test_that("arithmetic operations work properly", {
   # functional test
   z <- ts_df(ts_c(mdeaths, fdeaths) %ts/% ts_c(mdeaths, fdeaths))
   expect_is(z, "data.frame")
-  z <- ts_ts(ts_df(ts_c(mdeaths, fdeaths) %ts/% 1))
+  z <- ts_df(ts_c(mdeaths, fdeaths) %ts/% 1)
   expect_is(z, "data.frame")
-
-  )
 
 
 })
