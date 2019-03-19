4c0cfdd5e7a777bdcff730f712b5f94dad74a173
Christoph <christoph.sax@gmail.com>
2019-03-19 22:12:14 +0100

    ts_start() and ts_end() exported

--- R/tsbox-deprecated.R
+++ R/tsbox-deprecated.R
@@ -1,12 +1,17 @@
+#' Start and end of time series
+#'
+#' @inherit ts_dts
 #' @export
-#' @name ts_span
+#' @name tsbox-deprecated
 ts_start <- function(x) {
   .Deprecated("ts_summary")
   x.dts <- ts_dts(x)
   range(x.dts[[dts_cname(x.dts)$time]])[1]
 }
+
+
 #' @export
-#' @name ts_span
+#' @name tsbox-deprecated
 ts_end <- function(x) {
   .Deprecated("ts_summary")
   x.dts <- ts_dts(x)
