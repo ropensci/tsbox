6e17587d1cbcaf58f87687367f34c1c944772ebd
Stefan Fritsch <stefan.fritsch@stat-up.com>
2019-05-24 17:02:37 +0200

    Add test for special characters in names and the sep= parameter

--- tests/testthat/test_combine_cols_data.table.R
+++ tests/testthat/test_combine_cols_data.table.R
@@ -3,15 +3,41 @@ library(tsbox)
 
 context("Test combine_cols_data.table")
 
-test_that("Works with column names containing spaces",
+test_that("Works with column names containing spaces and special characters",
          {
            DT <-
              data.table::data.table(a = letters,
                                     "a b" = LETTERS,
+                                    "a;b" = rev(LETTERS),
                                     c = rnorm(length(letters)))
 
+           Output <- tsbox:::combine_cols_data.table(DT, c("a", "a b", "a;b"))
+
            expect_equivalent(
-             names(tsbox:::combine_cols_data.table(DT, c("a", "a b"))),
+             names(Output),
              c("id", "c")
            )
+
+           expect_equivalent(Output[1, id],
+                             "a_A_Z")
          })
+
+
+test_that("Parameter sep works",
+          {
+            DT <-
+              data.table::data.table(a = letters,
+                                     "a b" = LETTERS,
+                                     c = rnorm(length(letters)))
+
+            Output <- tsbox:::combine_cols_data.table(DT, c("a", "a b"),
+                                                      sep = " ")
+
+            expect_equivalent(
+              names(Output),
+              c("id", "c")
+            )
+
+            expect_equivalent(Output[1, id],
+                              "a A")
+          })
