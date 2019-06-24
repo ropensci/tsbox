8367b40fe222254c231f7b5f16bc1328df81e644
Christoph <christoph.sax@gmail.com>
2019-06-24 16:11:41 +0200

    test: invalid colnames are handled correctly

--- tests/testthat/test_nonstandard_cnames.R
+++ tests/testthat/test_nonstandard_cnames.R
@@ -25,6 +25,29 @@ test_that("main functions work with non standard cnames", {
 
 })
 
+test_that("invalid colnames are handled correctly", {
+
+  x <- ts_tbl(ts_c(mdeaths, AirPassengers))
+  x2 <- ts_tbl(ts_c(fdeaths, mdeaths))
+
+  names(x) <- c("Ö oe", "ha ha", "h h~dfsd")
+  names(x2) <- c("Ö oe", "ha ha", "h h~dfsd")
+
+  expect_is(ts_span(x, start = 1958), "tbl_df")
+  expect_is(ts_c(x, x2), "tbl_df")
+  expect_is(ts_bind(x, x2), "tbl_df")
+  expect_is(ts_scale(x), "tbl_df")
+  expect_is(ts_frequency(x, "year"), "tbl_df")
+  expect_is(ts_index(x2), "tbl_df")
+  expect_is(ts_lag(x), "tbl_df")
+  expect_is(ts_pick(x, 'mdeaths'), "tbl_df")
+
+  skip_on_cran()
+  expect_is(ts_plot(x), "call")
+
+})
+
+
 test_that("years are detected as time", {
   df <- data.frame(year = 2000:2009, value = 1:10)
   expect_equal(ts_summary(ts_ts(df))$end, as.Date("2009-01-01"))
