adda5a1e93646b62ffa4e89401b7b76f95c5c8f0
Christoph <christoph.sax@gmail.com>
2019-07-10 01:09:41 +0200

    support irts

---
+++ R/to_from_tseries.R
@@ -0,0 +1,36 @@
+register_class("irts")
+
+# to ---------------------------------------------------------------------------
+
+ts_irts_dts <- function(x) {
+  stopifnot(requireNamespace("tseries"))
+  x.dt <- ts_wide(ts_data.table(ts_default(x)))
+  time <- as.POSIXct(x.dt$time)
+  data <- as.matrix(x.dt[, setdiff(names(x.dt), "time"), with = FALSE])
+  tseries::irts(time, data)
+}
+
+
+# from -------------------------------------------------------------------------
+
+#' @export
+#' @method ts_dts irts
+ts_dts.irts <- function(x) {
+  stopifnot(requireNamespace("tseries"))
+  time <- as.POSIXct(x$time)
+  class(time) <- "POSIXct"  # need to loose POSIXt class
+  z <- data.table(time = time, x$value)
+  if (ncol(z) >= 3) z <- ts_long(z)
+  ts_dts(z)
+}
+
+
+# main converter ---------------------------------------------------------------
+
+#' @name ts_ts
+#' @export
+ts_irts <- function(x) {
+  stopifnot(ts_boxable(x))
+  if (relevant_class(x) == "irts") return(x)
+  ts_irts_dts(ts_dts(x))
+}
--- tests/testthat/test_auto.R
+++ tests/testthat/test_auto.R
@@ -1,33 +1,33 @@
 library(testthat)
 library(tsbox)
 
-# install.packages(c("tsibble", "xts", "timeSeries", "zoo", "tibbletime"))
+# install.packages(c("tsibble", "xts", "timeSeries", "zoo", "tibbletime", "tseries"))
 
 context("automated tests for all supported classes")
 
 
 test_that("two way conversion", {
 
-    skip_if_not_installed("tibbletime")
-    skip_if_not_installed("tsibble")
-    skip_if_not_installed("timeSeries")
-    skip_if_not_installed("zoo")
-    skip_if_not_installed("tis")
-
+  skip_if_not_installed("tibbletime")
+  skip_if_not_installed("tsibble")
+  skip_if_not_installed("timeSeries")
+  skip_if_not_installed("zoo")
+  skip_if_not_installed("tis")
 
   for (class in names(tsbox:::supported_classes())){
     message(class)
+
     ts_fun <- get(paste0("ts_", class))
 
     # single series
     expect_equal(ts_ts(ts_fun(AirPassengers)), AirPassengers)
 
     # tsibble alphabetically reorders key column, separate test below
-    if (class == "tsibble") break
+    if (class == "tsibble") next
 
     # tis: does not deal correctly with 'as.tis(EuStockMarkets)'
     # timeSeries: stored in seconds only, which prevents back covnersion to ts
-    if (!(class %in% c("timeSeries", "tis"))){
+    if (!(class %in% c("timeSeries", "tis", "irts"))){
       expect_equal(ts_ts(ts_fun(EuStockMarkets)), EuStockMarkets)
     }
 
