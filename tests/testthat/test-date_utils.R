8d00f3c38e2bbf0dabebd6038953ec788f307b67
Christoph Sax <christoph.sax@gmail.com>
2021-09-14 16:02:34 +0200

    rm unused date_ helpers

--- tests/testthat/test-date_utils.R
+++ tests/testthat/test-date_utils.R
@@ -4,61 +4,6 @@ library(tsbox)
 context("date utils")
 
 
-test_that("df aggregation using date_ functions is working", {
-
-  # 3 cols
-  library(dplyr)
-  x <- ts_tbl(ts_c(mdeaths, fdeaths)) %>%
-    mutate(time = date_year(time)) %>%
-    group_by(id, time) %>%
-    summarize(value = mean(value)) %>%
-    ungroup()
-
-  expect_equal(
-    x,
-    arrange(ts_tbl(ts_frequency(ts_c(mdeaths, fdeaths), "year")), id)
-  )
-
-
-  x <- ts_tbl(ts_c(mdeaths, fdeaths)) %>%
-    mutate(time = date_quarter(time)) %>%
-    group_by(id, time) %>%
-    summarize(value = mean(value)) %>%
-    ungroup()
-
-  expect_equal(
-    x,
-    arrange(ts_tbl(ts_frequency(ts_c(mdeaths, fdeaths), "quarter")), id)
-  )
-
-  x <- ts_bind(NA, ts_tbl(ts_c(EuStockMarkets)), NA) %>%
-    mutate(time = as.Date(date_month(time))) %>%
-    group_by(id, time) %>%
-    summarize(value = mean(value)) %>%
-    ungroup() %>%
-    ts_na_omit()
-  expect_equal(
-    x,
-    arrange(ts_tbl(ts_frequency(ts_c(EuStockMarkets), "month")), id)
-  )
-
-  # include incompletes
-  x <- ts_tbl(ts_c(EuStockMarkets)) %>%
-    mutate(time = as.Date(date_month(time))) %>%
-    group_by(id, time) %>%
-    summarize(value = mean(value)) %>%
-    ungroup()
-  expect_equal(
-    x,
-    arrange(
-      ts_tbl(ts_frequency(ts_c(EuStockMarkets), "month", na.rm = TRUE)),
-      id
-    )
-  )
-
-})
-
-
 test_that("time_shift is working", {
   x <- ts_tbl(ts_c(mdeaths, fdeaths))
   expect_equal(x$time, tsbox:::time_shift(x$time))
